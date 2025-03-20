e7a4768641bb92dd49fa6188a6849bfdd95b3b0e
nifemibosun <nifemibosun70@gmail.com>
2025-03-20 17:24:53 +0100

    Rewritting the compiler from scratch

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,3 @@
-mod ast;
-mod mini;
-mod parser;
-mod scanner;
-mod tokens;
-
-use std::env::args;
-use std::process;
-use mini::Mini;
-
 fn main() {
-    let args: Vec<String> = args().collect();
-    let mut mini = Mini::new();
-
-    if args.len() > 2 {
-        eprintln!("Usage: mini <filename>.mini");
-        process::exit(64);
-    } else if args.len() == 2 {
-        let filename = &args[1];
-        if let Err(err) = mini.run_file(filename) {
-            eprintln!("Error running file: {}", err);
-            process::exit(1);
-        }
-    } else {
-        if let Err(err) = mini.run_prompt() {
-            eprintln!("Error in REPL: {}", err);
-            process::exit(1);
-        }
-    }
+    println!("Welcome to mini-lang");
 }
