44edc0c60874d3310ac20ed379c24d7584fcaaed
Nifemi Bosun <nifemibosun70@gmail.com>
2025-09-25 23:05:43 +0100

    Started working on the structure of the parser(wrote some empty functions)

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,14 +3,51 @@
 pub mod ast;
 
 use crate::scanner::token::{Token, TokenType};
+use crate::parser::ast::*;
 
 pub struct Parser {
     tokens: Vec<Token>,
-    current: isize,
+    current: usize,
 }
 
 impl Parser {
     fn new(tokens: Vec<Token>) -> Self {
         Parser { tokens, current: 0 }
+    }
+
+    fn parse(&mut self) {
+        
+    }
+
+    fn expression(&mut self) {
+        
+    }
+
+    fn statement(&mut self) {
+
+    }
+
+    fn declaration(&mut self) {
+
+    }
+
+    fn advance(&mut self) {
+        
+    }
+
+    fn is_at_end(&self) {
+        
+    }
+
+    fn check(&mut self, t_type: TokenType) {
+        
+    }
+
+    fn match_token(&mut self, types: Vec<TokenType>) {
+        
+    }
+
+    fn peek(&self) {
+
     }
 }
